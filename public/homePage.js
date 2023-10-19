aff133f2e982109be5f637847191e4f40a7fddf4
Ivan Surovegin <givenrous@yandex.ru>
2023-10-19 11:10:14 +0300

    решение

---
+++ public/homePage.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => {
+  ApiConnector.logout((response) => {
+    if (response) {
+      location.reload();
+    }
+  });
+};
+
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const ratesBoard = new RatesBoard();
+
+function exchangeRateUpdate() {
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+};
+
+exchangeRateUpdate();
+
+setInterval(() => exchangeRateUpdate(), 60000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Баланс пополнен");
+    } else {
+      moneyManager.setMessage(response.success, response.error);
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    } else {
+      moneyManager.setMessage(response.success, response.error);
+    }
+  });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    } else {
+      moneyManager.setMessage(response.success, response.error);
+    }
+  });
+};
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(true, "Пользователь добавлен в список");
+    } else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+    } else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
+  });
+};
